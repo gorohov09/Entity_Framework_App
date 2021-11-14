33918e8066dc674735fb0c9535153a0484e2f3d4
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-11-14 23:14:32 +0300

    Создали базу данных

---
+++ Environmental_monitoring_DateBase_ADO5.Net/Create_DB.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE Source
+(
+	ID_Source int NOT NULL IDENTITY(1, 1),
+	Name_Source nvarchar(50) NULL,
+	Addres nvarchar(100) NULL,
+
+	CONSTRAINT PK_Source_IdSource PRIMARY KEY(ID_Source)
+);
+
+CREATE TABLE Emission
+(
+	ID_Emission int NOT NULL IDENTITY(1, 1),
+	ID_Source int NOT NULL,
+	Count_Emission float NULL,
+	Text_Emission nvarchar(100) NULL,
+	Date_Emission datetime NULL,
+
+	CONSTRAINT PK_Emission_IDEmission PRIMARY KEY(ID_Emission),
+	CONSTRAINT FK_Emission_IDSource FOREIGN KEY(ID_Source) REFERENCES Source(ID_Source)
+);
